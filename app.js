428ae70929831e4f551de86e584c3013c93aa59e
TooProForYou <95485139+TooProForYou@users.noreply.github.com>
2022-09-23 20:47:49 +0530

    Add files via upload

--- app.js
+++ app.js
@@ -1,10 +1,46 @@
 song_1 = " ";
 song_2 = " ";
+leftWristX = 0;
+leftWristY = 0;
+rightWristX = 0;
+rightWristY = 0;
 
 function preload()
 {
     song = loadSound("song1.mp3")
     song = loadSound("song2.mp3")
+}
+
+function setup()
+{
+    canvas = createCanvas(600,500);
+    canvas.center();
+
+    video = createCapture(VIDEO);
+    video.hide();
+    
+    poseNet = ml5.poseNet(video, modelLoaded);
+    poseNet.on('pose', gotPoses);
+}
+
+function modelLoaded()
+{
+    console.log('PoseNet Is Initialized');
+}
+
+function gotPoses(results)
+{
+    if(results.length > 0)
+    {
+        console.log(results);
+        leftWristX = results[0].pose.leftWrist.x;
+        leftWristY = results[0].pose.leftWrist.y;
+        console.log("leftWristX = " + leftWristX +" leftWristY = "+ leftWristY);
+
+        rightWristX = results[0].pose.rightWrist.x;
+        rightWristY = results[0].pose.rightWrist.y;
+        console.log("rightWristX = " + rightWristX +" rightWristY = "+ rightWristY);
+    }
 }
 
 function draw() {
@@ -17,6 +53,8 @@ function draw() {
 function play()
 {
     song.play();
+    song.setVolume(1);
+    song.rate(1);
 }
 
 function stop()
@@ -25,11 +63,3 @@ function stop()
 }
 
 
-function setup()
-{
-    canvas = createCanvas(600,500);
-    canvas.center();
-
-    video = createCapture(VIDEO);
-    video.hide();
-}
